d6e3709b6772a2ab4a0ea3a9530040adc36b0c7e
Rolin Blake <rolinmblake@gmail.com>
2023-09-05 22:29:25 -0500

    added more FX however pre-gain-view and pre-gain-slider are not displaying as desired once user uploads an audio file

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 let audioCtx: AudioContext | undefined = undefined;
+let masterGain: GainNode | undefined = undefined;
 let distNode: WaveShaperNode | undefined = undefined;
+let preGain: GainNode | undefined = undefined;
 let audioElement: HTMLAudioElement | undefined = undefined;
 let paused: boolean = true;
+
+document.getElementById('master-gain-slider')!.addEventListener('input', function() {
+  const slider = <HTMLInputElement> document.getElementById('master-gain-slider');
+  let val: number = slider.valueAsNumber;
+  masterGain!.gain.setValueAtTime(val, audioCtx!.currentTime);
+  document.getElementById('master-gain-view')!.innerHTML = 'Master Gain: '+val.toString();
+});
 
 function getDistortionCurve(amount: number): Float32Array {
   const k: number = typeof amount === 'number' ? amount : 50;
@@ -14,11 +23,39 @@ function getDistortionCurve(amount: number): Float32Array {
   return curve;
 }
 
+document.getElementById('distortion-amount-slider')!.addEventListener('input', function() {
+  const slider = <HTMLInputElement> document.getElementById('distortion-amount-slider');
+  let val: number = slider.valueAsNumber;
+  distNode!.curve = getDistortionCurve(val);
+  document.getElementById('distortion-amount-view')!.innerHTML = 'Distortion Amount: '+val.toString();
+});
+
+document.getElementById('pre-gain-slider')!.addEventListener('input', function() {
+  const slider = <HTMLInputElement> document.getElementById('pre-gain-slider');
+  let val: number = slider.valueAsNumber;
+  preGain!.gain.setValueAtTime(val, audioCtx!.currentTime);
+  document.getElementById('pre-gain-view')!.innerHTML = "Pre Gain: "+val.toString();
+});
+
 window.onload = function() {
   audioCtx = new AudioContext();
+
+  masterGain = audioCtx.createGain();
+  masterGain.gain.setValueAtTime(1, audioCtx.currentTime);
+  const masterGainSlider = <HTMLInputElement> document.getElementById('master-gain-slider');
+  masterGainSlider.value = '1';
+
   distNode = audioCtx.createWaveShaper();
   distNode.curve = getDistortionCurve(0);
   distNode.oversample = <OverSampleType>'2x';
+  const distSlider = <HTMLInputElement> document.getElementById('distortion-amount-slider');
+  distSlider.value = '0';
+
+  preGain = audioCtx.createGain();
+  preGain.gain.setValueAtTime(10, audioCtx.currentTime);
+  const preGainSlider = <HTMLInputElement> document.getElementById('pre-gain-slider');
+  preGainSlider.value = '10';
+
   const scrubInput = <HTMLInputElement> document.getElementById('audio-scrub-input');
   scrubInput.value = '0';
   scrubInput.addEventListener('input', function() {
@@ -44,22 +81,27 @@ function handleAudioUpload() {
       return;
     }
     const audioFile = audioFileInput.files[0];
-    const urlHeader = <HTMLHeadingElement> document.getElementById('audio-url-header');
-    urlHeader.innerHTML = 'Current .mp3 File: '+audioFile.name;
-    if (urlHeader.style.display === 'none') {
-      urlHeader.style.display = 'block';
-    }
     const audioWebUrl = URL.createObjectURL(audioFile);
     audioElement = <HTMLAudioElement> document.getElementById('main-audio');
     audioElement.src = audioWebUrl;
     audioElement.load();
-    const audioSourceNode = audioCtx!.createMediaElementSource(audioElement); // setting up FX routing for processing
-    audioSourceNode.connect(distNode!);
-    distNode!.connect(audioCtx!.destination);
-    const playPauseBtn = <HTMLButtonElement> document.getElementById('play-pause-btn');
-    playPauseBtn.style.display = 'block';
-    const scrubInput = <HTMLInputElement> document.getElementById('audio-scrub-input');
-    scrubInput.style.display = 'block';
+    const audioSourceNode = audioCtx!.createMediaElementSource(audioElement); // FX routing for processing
+    audioSourceNode.connect(preGain!).connect(distNode!).connect(masterGain!).connect(audioCtx!.destination);
+    const urlHeader = <HTMLHeadingElement> document.getElementById('audio-url-header');
+    urlHeader.innerHTML = 'Current .mp3 File: '+audioFile.name;
+    if (urlHeader.style.display === 'none') { // Making hidden elements visible
+      urlHeader.style.display = 'block';
+      document.getElementById('master-gain-view')!.style.display = 'block';
+      document.getElementById('master-gain-slider')!.style.display = 'block';
+      document.getElementById('distortion-amount-view')!.style.display = 'block';
+      document.getElementById('distortion-amount-slider')!.style.display = 'block';
+      document.getElementById('pre-gain-view')!.style.display = 'block';
+      document.getElementById('pre-gain-slider')!.style.display = 'block';
+      const playPauseBtn = <HTMLButtonElement> document.getElementById('play-pause-btn');
+      playPauseBtn.style.display = 'block';
+      const scrubInput = <HTMLInputElement> document.getElementById('audio-scrub-input');
+      scrubInput.style.display = 'block';
+    }
   } else {
     alert('Please select a .mp3 file to edit.');
     audioFileInput.value = '';
